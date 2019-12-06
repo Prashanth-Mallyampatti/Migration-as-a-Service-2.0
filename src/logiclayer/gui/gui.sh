a1a56b1f734e1f500e15e7ee130202581661016d
unknown <rvijaya4@ncsu.edu>
2019-12-06 11:39:22 -0500

    Adding tenant files

--- src/logiclayer/gui/gui.sh
+++ src/logiclayer/gui/gui.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-INFRA_DIR="/root/Migration-as-a-Service/src/northbound/config_files/infrastructure/"
-MIG_DIR="/root/Migration-as-a-Service/src/northbound/config_files/migration/"
-GUI_INFRA_DIR="/root/Migration-as-a-Service/src/northbound/gui/output/infra/*"
-GUI_MIG_DIR="/root/Migration-as-a-Service/src/northbound/gui/output/migrate/*"
+INFRA_DIR="/root/Migration-as-a-Service-2.0/src/northbound/config_files/infrastructure/"
+MIG_DIR="/root/Migration-as-a-Service-2.0/src/northbound/config_files/migration/"
+GUI_INFRA_DIR="/root/Migration-as-a-Service-2.0/src/northbound/gui/output/infra/*"
+GUI_MIG_DIR="/root/Migration-as-a-Service-2.0/src/northbound/gui/output/migrate/*"
 
-cp $GUI_INFRA_DIR $INFRA_DIR
-cp $GUI_MIG_DIR $MIG_DIR
+while [ 1 ]; do	
+	cp $GUI_INFRA_DIR $INFRA_DIR
+	cp $GUI_MIG_DIR $MIG_DIR
 
-cd /root/Migration-as-a-Service/src/northbound/config_files
-git add .
-git commit -m "Adding tenant files"
-git push origin master
+	cd /root/Migration-as-a-Service-2.0/src/northbound/config_files
+	git add .
+	git commit -m "Adding tenant files"
+	git push origin master
+
+	sleep 5
+done
