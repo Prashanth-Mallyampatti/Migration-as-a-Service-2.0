bf31b6c8238331f2c1068d088a98fdd3805981de
Prashanth-Mallyampatti <prashanth.1996@gmail.com>
2019-12-07 02:31:07 +0000

    kubernetes self healing feature

--- src/logiclayer/self_healing.py
+++ src/logiclayer/self_healing.py
@@ -2,45 +2,57 @@ import sys
 import os
 import subprocess
 import yaml
+import logging
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-#containers = subprocess.Popen("docker ps --all --format '{{.Names}}'", shell=True, stdout=subprocess.PIPE, universal_newlines=True).communicate()[0].rstrip()
+def polling():
+  print ("polling")
+  f = open("/root/Migration-as-a-Service-2.0/etc/migrating.txt")
+  is_migrating = f.read()
 
+  print (is_migrating)
 
-with open("/root/Migration-as-a-Service-2.0/src/northbound/config_files/infrastructure/" + str(sys.argv[2]), 'r') as file:
-  main_yaml_content = yaml.safe_load(file)
-  print (main_yaml_content)
+  if is_migrating.rstrip() == "0":
+    with open("/root/Migration-as-a-Service-2.0/src/northbound/config_files/infrastructure/" + str(sys.argv[1]) + ".yml", 'r') as file:
+      main_yaml_content = yaml.safe_load(file)
+      print (main_yaml_content)
 
-  print (main_yaml_content["MISC"])
+      print (main_yaml_content["MISC"])
 
-  with open("/root/Migration-as-a-Service-2.0/etc/" + str(sys.argv[1]) + "/" + str(sys.argv[2]), 'r') as file:
-    yaml_content = yaml.safe_load(file)
+      with open("/root/Migration-as-a-Service-2.0/etc/" + str(sys.argv[1]) + "/" + str(sys.argv[1]) + "c1.yml", 'r') as file:
+        yaml_content = yaml.safe_load(file)
 
-  #print (yaml_content)
+      #print (yaml_content)
 
-  containers = subprocess.Popen("docker ps --all --format '{{.Names}}'", shell=True, stdout=subprocess.PIPE)
-  #print (containers)
+      containers = subprocess.Popen("docker ps --all --format '{{.Names}}'", shell=True, stdout=subprocess.PIPE)
+      #print (containers)
 
-  for line in iter(containers.stdout.readline,''):
-    #print (line.rstrip())
-    pid = subprocess.Popen("docker inspect --format '{{.State.Pid}}' " + str(line.rstrip()), shell=True, stdout=subprocess.PIPE, universal_newlines=True).communicate()[0].rstrip()
-    #print ("printing pid")
-    #print(pid)
-    if pid is "0":
-      for ns in yaml_content["Namespace"]:
-        #print (ns["name"])
-        if str(line.rstrip()) == str(ns["name"]):
-          print ("starting namespace container")
-          exit_status = os.system("docker start " + str(line.rstrip()))
-          break
-      for sub in yaml_content["Subnet"]:
-        if str(line.rstrip()) == str(sub["ns_name"]):
-          print ("starting subnet conatiner")
-          exit_status = os.system("docker start " + str(line.rstrip()))
-          break
-        #print (sub["vms"])
-        for vm in sub["vms"]:
-          #print (vm["name"])
-          print ("starting vm container")
-          if str(line.rstrip()) == str(vm["name"]):
-            exit_status = os.system("docker start " + str(line.rstrip()))
-            break
+      for line in iter(containers.stdout.readline,''):
+        #print (line.rstrip())
+        pid = subprocess.Popen("docker inspect --format '{{.State.Pid}}' " + str(line.rstrip()), shell=True, stdout=subprocess.PIPE, universal_newlines=True).communicate()[0].rstrip()
+        #print ("printing pid")
+        #print(pid)
+        if pid is "0":
+          for ns in yaml_content["Namespace"]:
+            #print (ns["name"])
+            if str(line.rstrip()) == str(ns["name"]):
+              print ("starting namespace container")
+              exit_status = os.system("docker start " + str(line.rstrip()))
+              break
+          for sub in yaml_content["Subnet"]:
+            if str(line.rstrip()) == str(sub["ns_name"]):
+              print ("starting subnet conatiner")
+              exit_status = os.system("docker start " + str(line.rstrip()))
+              break
+            #print (sub["vms"])
+            for vm in sub["vms"]:
+              #print (vm["name"])
+              print ("starting vm container")
+              if str(line.rstrip()) == str(vm["name"]):
+                exit_status = os.system("docker start " + str(line.rstrip()))
+                break
+
+print ("running for every minute")
+scheduler = BlockingScheduler()
+scheduler.add_job(polling, 'interval', seconds=10)
+scheduler.start()
